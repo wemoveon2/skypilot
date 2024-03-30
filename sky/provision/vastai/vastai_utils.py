e743e7f845a0481e0187e344e264d2005a75eb2b
Alan Yu <inv.alanyu2000@gmail.com>
2024-03-30 04:21:08 -0400

    fix client

--- sky/provision/vastai/vastai_utils.py
+++ sky/provision/vastai/vastai_utils.py
@@ -1,6 +1,7 @@
 """FluidStack API client."""
 
 import json
+import csv
 import uuid
 from pathlib import Path
 
@@ -11,7 +12,7 @@ def get_key_suffix():
     return str(uuid.uuid4()).replace('-', '')[:8]
 
 
-ENDPOINT = Path('https://console.vast.ai/api/v0')
+ENDPOINT = 'https://console.vast.ai/api/v0/'
 VASTAI_API_KEY_PATH = Path('~/.vastai/api_key').expanduser()
 
 
@@ -45,5 +46,47 @@ def raise_vastai_error(response: requests.Response) -> None:
 class VastAIClient:
     """Vast AI API Client"""
 
-    def __init__(self):
-        self.api_key = read_contents(VASTAI_API_KEY_PATH)
+    def __init__(self, endpoint = ENDPOINT, key_path = VASTAI_API_KEY_PATH):
+        self.api_key = read_contents(key_path)
+        self.endpoint = endpoint
+    
+    def get_instances(self): 
+      response = requests.get(self.endpoint + 'bundles', auth=('Key', self.api_key), params={
+      "verified": {"eq": True},
+      "rentable": {"eq": True},
+      "order": [["dphtotal","asc"],["total_flops","asc"]],
+      "cuda_max_good": {"gte":12.1},
+      "limit": 192
+    })
+      raise_vastai_error(response)
+      offers: dict = response.json()
+      parsed_offers = list()
+      for offer in offers['offers']:
+        parsed_offers.append(self.__parse_offer(offer))
+      return parsed_offers
+
+    def __parse_offer(self, offer) -> dict:
+      def get_gpu_info(offer) -> str: 
+        return json.dumps({
+        'Gpus': [{
+            'Name': offer['gpu_name'],
+            'Manufacturer': offer.get('gpu_arch', 'unk').upper(),
+            'Count': offer['num_gpus'],
+            'MemoryInfo': {
+                'SizeInMiB': offer['gpu_ram']
+            }
+          }],
+        'TotalGpuMemoryInMiB': float(offer['gpu_ram']) * int(offer['num_gpus'])
+        })
+      return {
+          "InstanceType": f"{offer['cpu_name']}_{offer['gpu_name']}".replace(" ", "_").lower(),
+          "AcceleratorName": offer["gpu_name"].replace(" ", ""),
+          "AcceleratorCount": str(offer["num_gpus"]).strip(),
+          "vCPUs": offer["cpu_cores"],
+          "MemoryGiB": offer["cpu_ram"] // 1024,  # Convert MiB to GiB
+          "Price": offer["dph_total"],
+          "Region": offer["geolocation"],
+          "GpuInfo": get_gpu_info(offer),
+          "SpotPrice": offer["min_bid"],
+        } 
+        
